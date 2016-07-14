cf0a82eb4d9ff4c82de1f18e2f5744edb6e6590a
Jiří Zůna <jiri@zunovi.cz>
2016-07-14 12:51:35 +0200

    MOD user fnx-input as a wrapper for fnx-text component

--- ng2/lib/src/components/input/fnx_input.dart
+++ ng2/lib/src/components/input/fnx_input.dart
@@ -1,22 +1,76 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/common.dart';
+import 'package:fnx_ui/src/util/ui.dart' as ui;
+import 'dart:async';
 
 @Component(
     selector: 'fnx-input',
-    templateUrl: 'fnx_input.html'
-)
+    template: r'''
+<div class="input">
+  <label *ngIf="label != null" for="{{ componentId }}">{{ label }}</label>
+  <ng-content></ng-content>
+  <label *ngIf="hasError() && errorMessage != null" class="error" for="{{ componentId }}">{{ errorMessage }}</label>
+</div>
+''')
 class FnxInput {
 
-  dynamic _value;
-  @Output()
-  EventEmitter valueChange = new EventEmitter();
+  final String componentId = ui.uid('comp_');
 
-  @Input()
-  set value(dynamic val) {
-    _value = val;
-    valueChange.emit(val);
+  FnxInputComponent _component;
+
+
+  @Input() String label;
+  @Input() String errorMessage;
+
+  bool error = false;
+
+  StreamSubscription _errorSubscription;
+
+  bool hasError() {
+    return error;
   }
-  get value => _value;
+
+  FnxInputComponent get component => _component;
+
+  void set component(FnxInputComponent component) {
+    if (_errorSubscription != null) _errorSubscription.cancel();
+    this._component = component;
+    if (component != null) {
+      _errorSubscription = component.errorStateChange.listen(handleErrorChange);
+    }
+  }
+
+  handleErrorChange(dynamic errorStatus) {
+    if (errorStatus is bool) {
+      this.error = errorStatus;
+    }
+  }
+}
+
+abstract class FnxInputComponent implements OnInit {
+
+  final FnxInput _wrapper;
+
+  final String _privComponentId = ui.uid("comp_");
+
+  final EventEmitter errorStateChange = new EventEmitter();
+
+  FnxInputComponent(this._wrapper);
+
+  String get componentId {
+    if (_wrapper != null) {
+      return _wrapper.componentId;
+    } else {
+      return _privComponentId;
+    }
+  }
+
+  @override
+  ngOnInit() {
+    if (_wrapper != null) {
+      _wrapper.component = this;
+    }
+  }
 }
 
 const FNX_INPUT_DIRECTIVES = const [FnxInput];
--- ng2/lib/src/components/input/fnx_text.dart
+++ ng2/lib/src/components/input/fnx_text.dart
@@ -1,36 +1,31 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/common.dart';
 import 'package:fnx_ui/src/util/ui.dart';
+import 'package:fnx_ui/fnx_ui.dart';
+import 'package:fnx_ui/src/components/input/fnx_input.dart';
 
 
 const CUSTOM_INPUT_TEXT_VALUE_ACCESSOR = const Provider(  NG_VALUE_ACCESSOR,
                                                           useExisting: FnxText,
                                                           multi: true);
 
-
 @Component(
     selector: 'fnx-text',
-    templateUrl: 'fnx_text.html',
+    template: r'''
+<input id="{{ componentId }}" type="text" [(ngModel)]="value"/>
+''',
     providers: const [CUSTOM_INPUT_TEXT_VALUE_ACCESSOR]
 )
-class FnxText implements ControlValueAccessor {
-
-  final String id = uid('input_');
+class FnxText extends FnxInputComponent implements ControlValueAccessor, OnInit {
 
   dynamic _value;
-
-  @Input()
-  String label = null;
-
-  @Input()
-  String errorMsg = null;
+  @Output() EventEmitter valueChange = new EventEmitter();
 
   bool error = false;
 
-  @Output()
-  EventEmitter valueChange = new EventEmitter();
-
   @ContentChild(NgFormControl) NgFormControl state;
+
+  FnxText(@Optional() FnxInput wrapper): super(wrapper);
 
   get value => _value;
 
@@ -44,11 +39,17 @@ class FnxText implements ControlValueAccessor {
     }
   }
 
+  @override
+  ngOnInit() {
+    super.ngOnInit();
+  }
+
   var onChange = (_) {};
   var onTouched = (_) {};
 
   void checkErrors() {
     error = state != null && !state.valid;
+    errorStateChange.emit(error);
   }
 
   @override
