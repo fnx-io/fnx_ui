b3ab4988fa1b18b8c82051cc298a8a121ffc9bab
Tomucha <tomucha@gmail.com>
2016-07-15 16:44:17 +0200

    Build for angular.

--- build_stylesheet.sh
+++ build_stylesheet.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Building stylesheet and copying to ng2 lib directory ..."; 
-cd stylesheet/ && gulp release && cp -r _dist/css ../ng2/lib/;
+cd stylesheet/ && gulp release && cp -r _dist/css ../lib/;
 cd ..;
