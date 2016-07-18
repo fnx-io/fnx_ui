2e0024ed576d8a9eb1824a50bdd8b49038c30618
Tomucha <tomucha@gmail.com>
2016-07-18 16:15:07 +0200

    Buttonish variant of dropdown.

--- stylesheet/deploy-demo.sh
+++ stylesheet/deploy-demo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 gulp release;
 gcloud --project ma-web compute copy-files _dist/* static-ma:/usr/share/nginx/www/demo.fnx.io/fnx_ui/css --zone europe-west1-b;
-cvrk.sh "Deploynuta nova verze fbx_ui viz: http://demo.fnx.io/fnx_ui/css/";
+cvrk.sh "Deploynuta nova verze fnx_ui viz: http://demo.fnx.io/fnx_ui/css/";
