fde2653b62947130a44710671d26f155ccd399dd
Tomucha <tomucha@gmail.com>
2016-08-26 21:59:39 +0200

    Added lazily loading table example into cookbook.

---
+++ example/cookbook/cookbook_table.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import '../example_app.dart';
+import 'package:angular2/core.dart';
+import 'package:logging/logging.dart';
+import 'package:angular2/common.dart';
+import 'package:fnx_ui/src/components/modal/fnx_modal.dart';
+import 'package:fnx_ui/src/components/app/fnx_app.dart';
+import '../example_buttons_renderer.dart';
+
+@Component(
+    selector: 'cookbook-table',
+    templateUrl: 'cookbook_table.html',
+    directives: const [ExampleButtonsRenderer]
+)
+class CookbookTable {
+
+  ExampleApp app;
+
+  CookbookTable(this.app);
+
+  ngOnInit() {
+    loadMore();
+  }
+
+  List get list => app.list;
+
+  void loadMore() {
+    app.loadMore();
+  }
+
+
+}
--- example/example_app.dart
+++ example/example_app.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'cookbook/cookbook_table.dart';
 import 'package:angular2/core.dart';
 import 'package:logging/logging.dart';
 import 'package:angular2/common.dart';
@@ -29,7 +30,8 @@ import 'example_form.dart';
   const Route(path: "/Modals", name: "Modals", component: ExampleModals),
   const Route(path: "/Tabs/...", name: "Tabs", component: ExampleTabs),
 
-  const Route(path: "/Cookbook/Preloader", name: "Cookbook-Preloader", component: CookbookPreloader)
+  const Route(path: "/Cookbook/Preloader", name: "Cookbook-Preloader", component: CookbookPreloader),
+  const Route(path: "/Cookbook/Table", name: "Cookbook-Table", component: CookbookTable)
 ])
 class ExampleApp {
 
@@ -51,7 +53,11 @@ class ExampleApp {
     "Velvet",
     "Underground",
     "Tenacious",
-    "D"
+    "D",
+    "Jefferson",
+    "Airplane",
+    "Rolling",
+    "Stones"
   ];
 
 
