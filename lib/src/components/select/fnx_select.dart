fnx-io/fnx_ui

commit fea983c25062d0591daf6cf6e34636a4f180bba7

MOD handle selection in FnxOption
We changed, how options are rendered, and so the logic had to be changed too

lib/src/components/select/fnx_option.html
@@ -1,4 +1,7 @@
-<div class="item">
+<div class="item"
+     (click)="optionSelected($event)"
+     [class.selected]="selected"
+     attr.data-prefix="{{selected ? 'done' : null}}">
     <template [ngIf]="label != null">{{label}}</template>
     <template [ngIf]="label == null"><ng-content></ng-content></template>
 </div>

lib/src/components/select/fnx_select.dart
@@ -141,8 +141,7 @@ class FnxSelect extends FnxInputComponent implements ControlValueAccessor, OnIni
     }
   }
 
-  void selectOption(dynamic value, Event event) {
-    event.stopPropagation();
+  void selectOption(dynamic value) {
     doSelectOption(value);
   }
 
@@ -339,6 +338,14 @@ class FnxOption implements OnInit {
   ngOnInit() {
     parent.options.add(new FnxOptionValue(value, label));
   }
+
+  void optionSelected(Event event) {
+    event?.preventDefault();
+
+    parent?.selectOption(value);
+  }
+
+  get selected => parent != null && parent.isSelected(value);
 }
 
 const FNX_SELECT_DIRECTIVES = const [FnxSelect, FnxOption];

lib/src/components/select/fnx_select.html
@@ -15,16 +15,6 @@
         </div>
         <div class="select__items">
             <ng-content></ng-content>
-            <!--
-            <div *ngFor="let opt of filteredOptions" (click)="selectOption(opt.value, $event)"
-                class="clickable item"
-                [class.selected]="isSelected(opt.value)" [class.highlighted]="isHighlighted(opt)">
-                <i *ngIf="multi" class="mi">
-                    <span *ngIf="isSelected(opt.value)">done</span>
-                </i>
-                {{ opt.label }}
-            </div>
-            -->
         </div>
         <p *ngIf="options.isEmpty" class="padding--small no-margin">
             {{ optionsEmptyLabel }}

commit f9b29c9d3742ca7f7596fd71917c6444dfc8aa7c

FIX filtering in fnx-select
Broken by fea983c25062d0591daf6cf6e34636a4f180bba7

We need to conditionally filter fnx-options, when user used an filter
inside the select. Since we allow arbitrary content in the fnx-option I
have changed the logic of rendering options.
This change will hide all fnx-option components which do not satisfy the
filter, if any is used.

lib/src/components/select/fnx_option.html
@@ -1,7 +1,8 @@
 <div class="item"
      (click)="optionSelected($event)"
      [class.selected]="selected"
-     attr.data-prefix="{{selected ? 'done' : null}}">
+     attr.data-prefix="{{selected ? 'done' : null}}"
+     [hidden]="!visible">
     <template [ngIf]="label != null">{{label}}</template>
     <template [ngIf]="label == null"><ng-content></ng-content></template>
 </div>

lib/src/components/select/fnx_select.dart
@@ -184,7 +184,7 @@ class FnxSelect extends FnxInputComponent implements ControlValueAccessor, OnIni
       var result = options.where((option) {
         return option.label.toLowerCase().contains(filter);
       }).toList();
-      _cachedFilteredOptions = null;
+      _cachedFilteredOptions = result;
       return result;
     } else {
       return options;
@@ -274,11 +274,11 @@ class FnxSelect extends FnxInputComponent implements ControlValueAccessor, OnIni
 }
 
 class FnxOptionValue {
-  final String id = uid('ov_');
+  final String id;
   final String label;
   final String value;
 
-  FnxOptionValue(this.value, this.label);
+  FnxOptionValue(this.id, this.value, this.label);
 
   @override
   bool operator ==(Object other) {
@@ -306,6 +306,8 @@ class FnxOptionValue {
 )
 class FnxOption implements OnInit {
 
+  final String id = uid('ov_');
+
   FnxSelect parent;
 
   @Input()
@@ -316,9 +318,18 @@ class FnxOption implements OnInit {
 
   FnxOption(this.parent);
 
+  bool get visible {
+    List<FnxOptionValue> opts = parent?.filteredOptions;
+    if (opts == null) return false;
+    FnxOptionValue found = opts.firstWhere((FnxOptionValue el) {
+      return el != null && el.id == id;
+    }, orElse: () => null);
+    return found != null;
+  }
+
   @override
   ngOnInit() {
-    parent.options.add(new FnxOptionValue(value, label));
+    parent.options.add(new FnxOptionValue(id, value, label));
   }
 
   void optionSelected(Event event) {