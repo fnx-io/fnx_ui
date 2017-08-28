1619775422e2cfa87d154317d2a3634c5a5c51ca
Honza Bittner <git@honzabittner.cz>
2017-08-28 16:33:54 +0200

    Add html code - preview toggling button to rich wysiwyg #16

--- lib/src/components/input/fnx_wysiwyg_rich.dart
+++ lib/src/components/input/fnx_wysiwyg_rich.dart
@@ -4,38 +4,42 @@ import 'dart:js';
 
 import 'package:angular2/common.dart';
 import 'package:angular2/core.dart';
+
 import 'package:fnx_ui/fnx_ui.dart';
 import 'package:fnx_ui/src/components/input/fnx_input.dart';
 import 'package:fnx_ui/src/quill/quill.dart';
 
-
 const EMPTY_STRING_VALUE = "<p><br></p>";
 
-const CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR = const Provider(  NG_VALUE_ACCESSOR,
-    useExisting: FnxWysiwygRich,
-    multi: true);
+const CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR =
+    const Provider(NG_VALUE_ACCESSOR, useExisting: FnxWysiwygRich, multi: true);
+
 @Component(
     selector: 'fnx-wysiwyg-rich',
     template: r'''
     <div class="wysiwyg--wrapper">
       <div #toolbar>
-          <span class="pointer char bold" (mousedown)="doFormatLine('header', 1)">H1</span>
-          <span class="pointer char bold fs-s" (mousedown)="doFormatLine('header', 2)">H2</span>
-          <span class="pointer char bold fs-xs" (mousedown)="doFormatLine('header', 3)">H3</span>
-          <span class="pointer char fs-xs" (mousedown)="doFormatLine('header', false)">P</span>
+          <span class="icon-container" [class.is--html]="htmlView == true">
+            <span class="pointer char bold" (mousedown)="doFormatLine('header', 1)">H1</span>
+            <span class="pointer char bold fs-s" (mousedown)="doFormatLine('header', 2)">H2</span>
+            <span class="pointer char bold fs-xs" (mousedown)="doFormatLine('header', 3)">H3</span>
+            <span class="pointer char bold fs-xs" (mousedown)="doFormatLine('header', false)">P</span>
+  
+            <span class="pointer icon margin--small--left" (mousedown)="doFormatText('bold')">format_bold</span>
+            <span class="pointer icon" (mousedown)="doFormatText('italic')">format_italic</span>
+  
+            <span class="pointer icon margin--small--left" (mousedown)="doFormatLine('align', false)">format_align_left</span>
+            <span class="pointer icon" (mousedown)="doFormatLine('align', 'center')">format_align_center</span>
+            <span class="pointer icon" (mousedown)="doFormatLine('align', 'right')">format_align_right</span>
+  
+            <span class="pointer icon margin--small--left" (mousedown)="doFormatLine('list', 'bullet')">format_list_bulleted</span>
+            <span class="pointer icon" (mousedown)="doFormatLine('list', 'ordered')">format_list_numbered</span>
+  
+            <span class="pointer icon margin--small--left" (mousedown)="doCreateLink()">link</span>
+            <span *ngIf="imagePicker != null" class="pointer icon margin--small--left" (mousedown)="doCreateImage()">insert_photo</span>
+          </span>
 
-          <span class="pointer icon margin--small--left" (mousedown)="doFormatText('bold')">format_bold</span>
-          <span class="pointer icon" (mousedown)="doFormatText('italic')">format_italic</span>
-
-          <span class="pointer icon margin--small--left" (mousedown)="doFormatLine('align', false)">format_align_left</span>
-          <span class="pointer icon" (mousedown)="doFormatLine('align', 'center')">format_align_center</span>
-          <span class="pointer icon" (mousedown)="doFormatLine('align', 'right')">format_align_right</span>
-
-          <span class="pointer icon margin--small--left" (mousedown)="doFormatLine('list', 'bullet')">format_list_bulleted</span>
-          <span class="pointer icon" (mousedown)="doFormatLine('list', 'ordered')">format_list_numbered</span>
-
-          <span class="pointer icon margin--small--left" (mousedown)="doCreateLink()">link</span>
-          <span *ngIf="imagePicker != null" class="pointer icon margin--small--left" (mousedown)="doCreateImage()">insert_photo</span>
+          <span class="pointer icon margin--small--left" (mousedown)="toggleView()">code</span>
 
           <!--<span class="pointer icon margin&#45;&#45;left" (mousedown)="doUndo()">undo</span>-->
 
@@ -43,10 +47,17 @@ const CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR = const Provider(  NG_VALUE_ACCES
       </div>
       <div class="input--component" [style.max-width]="maxWidth" [style.height]="height">
         <div #wysiwyg
+             [hidden]="htmlView == true"
              [class.readonly]="isReadonly"
              (blur)="edited()"
              (keyup)="edited()"
              ></div>
+
+         <fnx-textarea [(ngModel)]="value"
+                       (ngModelChange)="updateText($event)"
+                       [hidden]="htmlView == false" 
+                       [class.readonly]="isReadonly"
+                       ></fnx-textarea>
      </div>
    </div>
 ''',
@@ -55,29 +66,35 @@ const CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR = const Provider(  NG_VALUE_ACCES
       ":host .input--component { padding: 0;}",
       ":host .input--component a {text-decoration: underline;}",
       ":host .ql-container.ql-snow {border:0;}",
-      ":host .ql-toolbar.ql-snow {display: none;}"
+      ":host .ql-toolbar.ql-snow {display: none;}",
+      ":host fnx-textarea, :host fnx-textarea textarea {min-width: 100%; width: 100%; max-width: 100%; min-height: 100%; height: 100%; max-height: 100%;}",
+      ":host .icon-container.is--html {color: #777 !important; pointer-events: none;}"
     ],
-    providers: const [
-      CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR,
-      const Provider(Focusable, useExisting: FnxWysiwygRich)
-    ],
-    preserveWhitespace: false
-)
+    providers: const [CUSTOM_INPUT_WYSIWYG_RICH_VALUE_ACCESSOR, const Provider(Focusable, useExisting: FnxWysiwygRich)],
+    preserveWhitespace: false)
 class FnxWysiwygRich extends FnxInputComponent implements ControlValueAccessor, OnInit, OnDestroy, Focusable {
-
-  @Input() bool required = false;
-  @Input() bool readonly = false;
-  @Input() String height = "15";
-  @Input() String maxWidth = "50";
-  @Input() PickImageUrl imagePicker = null;
-  @Input() bool safe = true;
+  @Input()
+  bool required = false;
+  @Input()
+  bool readonly = false;
+  @Input()
+  String height = '15';
+  @Input()
+  String maxWidth = '50';
+  @Input()
+  PickImageUrl imagePicker = null;
+  @Input()
+  bool safe = true;
 
   Quill quill;
 
-  @ViewChild("wysiwyg")
+  @Input('initWithHtmlView')
+  bool htmlView = false;
+
+  @ViewChild('wysiwyg')
   ElementRef wysiwyg;
 
-  @ViewChild("toolbar")
+  @ViewChild('toolbar')
   ElementRef toolbar;
 
   Element get editor => ((wysiwyg.nativeElement as Element).firstChild as Element);
@@ -93,7 +110,7 @@ class FnxWysiwygRich extends FnxInputComponent implements ControlValueAccessor, 
   @override
   ngOnInit() {
     super.ngOnInit();
-    (wysiwyg.nativeElement as Element).innerHtml = value == null ? "" : value;
+    (wysiwyg.nativeElement as Element).innerHtml = value == null ? '' : value;
 
     QuillOptionsStatic options = new QuillOptionsStatic();
     options.theme = "snow";
@@ -115,76 +132,105 @@ class FnxWysiwygRich extends FnxInputComponent implements ControlValueAccessor, 
 
   void writeValue(obj) {
     super.writeValue(obj);
-    quill.pasteHTML(value == null ? "" : value);
+    quill.pasteHTML(value == null ? '' : value);
     if (safe) {
-      ((wysiwyg.nativeElement as Element).firstChild as Element).innerHtml = value == null ? "" : value;
-      
+      ((wysiwyg.nativeElement as Element).firstChild as Element).innerHtml = value == null ? '' : value;
     } else {
-      ((wysiwyg.nativeElement as Element).firstChild as Element).setInnerHtml(value == null ? "" : value, treeSanitizer: NodeTreeSanitizer.trusted);
+      ((wysiwyg.nativeElement as Element).firstChild as Element)
+          .setInnerHtml(value == null ? '' : value, treeSanitizer: NodeTreeSanitizer.trusted);
     }
   }
 
-  void doFormatText(String command, [var param = true]) {
-    RangeStatic range = quill.getSelection();
-    if (range != null) {
-      JsObject format = new JsObject.fromBrowserObject(quill.getFormat(range.index, range.length));
-      if (format.hasProperty("o")) {
+  void toggleView() {
+    htmlView ??= false;
+    htmlView = !htmlView;
+  }
+
+  Future doFormatText(String command, [var param = true]) async {
+    RangeStatic selection = quill.getSelection();
+
+    if (selection != null) {
+      JsObject format = new JsObject.fromBrowserObject(quill.getFormat(selection.index, selection.length));
+      if (format.hasProperty('o')) {
         format = format['o'];
       }
-      if ( format != null && format.hasProperty(command)) {
-        quill.formatText(range.index, range.length, command, false);
+      if (format != null && format.hasProperty(command)) {
+        quill.formatText(selection.index, selection.length, command, false);
       } else {
-        quill.formatText(range.index, range.length, command, param);
+        quill.formatText(selection.index, selection.length, command, param);
       }
-      edited();
-      focus().then((_) => restoreSelection(range));
     }
-  }
 
-  void doFormatLine(dynamic command, dynamic param) {
-    RangeStatic selection = quill.getSelection();
-    RangeStatic range = quill.getSelection();
-    if (range != null) {
-      quill.formatLine(range.index, range.length, command, param);
-    }
     edited();
-    focus().then((_) => restoreSelection(selection));
+    await focus();
+    restoreSelection(selection);
   }
 
+  Future doFormatLine(dynamic command, dynamic param) async {
+    RangeStatic selection = quill.getSelection();
+
+    if (selection != null) {
+      quill.formatLine(selection.index, selection.length, command, param);
+    }
+
+    edited();
+    await focus();
+    restoreSelection(selection);
+  }
 
   Future<Null> doCreateLink() async {
     RangeStatic selection = quill.getSelection();
+
     String link = await app.input("Insert link, including http://", headline: "Link");
+
     if (link != null) {
-      restoreSelection(selection);
-      doFormatText('link', link);
+      if (selection != null) {
+        restoreSelection(selection);
+        doFormatText('link', link);
+      } else {
+        quill.insertText(0, link);
+        quill.setSelection(0, link.length);
+        doFormatText('link', link);
+      }
     }
+
     edited();
-    focus().then((_) => restoreSelection(selection));
+    await focus();
+    restoreSelection(selection);
+  }
+
+  void updateText(String value) {
+    quill.pasteHTML(value);
   }
 
   Future<Null> doCreateImage() async {
-    RangeStatic range = quill.getSelection();
-    if (range != null) {
-      String url = await imagePicker();
-      if (url != null) {
-        quill.insertEmbed(range.index, 'image', url);
-        /*
-        var delta = new JsObject.jsify({"ops":[{
-          "format": {
-            "index": range.index,
-            "length": 1
-          },
-          "attributes": {
-            "width": 100
-          }
-        }]});
-        quill.updateContents(delta);
-        */
+    RangeStatic selection = quill.getSelection();
+
+    String url = await imagePicker();
+
+    if (url != null) {
+      if (selection != null) {
+        quill.insertEmbed(selection.index, 'image', url);
+      } else {
+        quill.setSelection(0, 0);
+        quill.insertEmbed(selection.index, 'image', url);
       }
+      /*
+      var delta = new JsObject.jsify({"ops":[{
+        "format": {
+          "index": range.index,
+          "length": 1
+        },
+        "attributes": {
+          "width": 100
+        }
+      }]});
+      quill.updateContents(delta);
+      */
     }
+
     edited();
-    focus();
+    await focus();
   }
 
   Future<bool> focus() async {
@@ -194,9 +240,10 @@ class FnxWysiwygRich extends FnxInputComponent implements ControlValueAccessor, 
   }
 
   void restoreSelection(RangeStatic savedSel) {
-    quill.setSelection(savedSel.index, savedSel.length);
+    if (savedSel != null) {
+      quill.setSelection(savedSel.index, savedSel.length);
+    }
   }
-
 }
 
 typedef Future<String> PickImageUrl();
