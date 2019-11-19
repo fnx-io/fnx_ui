e393fcb16d0cec5ab3b10afeaccd297215103b3c
Tomucha <tomucha@gmail.com>
2019-11-19 15:00:00 +0100

    Tooltip netrva na HTML elemtnu.

--- fnx_ui/lib/src/components/fnx_tooltip.dart
+++ fnx_ui/lib/src/components/fnx_tooltip.dart
@@ -6,19 +6,16 @@ import 'package:angular/angular.dart';
 ///
 /// One item to reorder must be a child of reorderContainerVertical.
 ///
-/// 
 ///
-@Directive(
-    selector: "[fnxTooltip]"
-)
+///
+@Directive(selector: "[fnxTooltip]")
 class FnxTooltip implements OnInit, OnDestroy {
-
   @Input()
   Object fnxTooltip;
 
-  HtmlElement itemElement;
+  Element itemElement;
 
-  HtmlElement currentTooltip;
+  Element currentTooltip;
 
   bool mouseIn;
   bool dead = false;
@@ -26,8 +23,7 @@ class FnxTooltip implements OnInit, OnDestroy {
   StreamSubscription<MouseEvent> subsEnter;
   StreamSubscription<MouseEvent> subsExit;
 
-  FnxTooltip(this.itemElement) {
-  }
+  FnxTooltip(this.itemElement) {}
 
   @override
   void ngOnDestroy() {
@@ -43,15 +39,14 @@ class FnxTooltip implements OnInit, OnDestroy {
     subsExit = itemElement.onMouseLeave.listen(mouseLeave);
   }
 
-
   void mouseEnter(MouseEvent event) async {
     mouseIn = true;
     await new Future.delayed(new Duration(milliseconds: 300));
     if (mouseIn && currentTooltip == null && fnxTooltip != null && !dead) {
       currentTooltip = new DivElement();
       currentTooltip.classes.add("arrow_box");
-      currentTooltip.style.position="absolute";
-      currentTooltip.style.zIndex="100000";
+      currentTooltip.style.position = "absolute";
+      currentTooltip.style.zIndex = "100000";
       setTooltipContent(fnxTooltip);
       document.body.children.add(currentTooltip);
       calculateTooltipPosition();
@@ -68,52 +63,53 @@ class FnxTooltip implements OnInit, OnDestroy {
     var parentBoundingBox = itemElement.getBoundingClientRect();
 
     currentTooltip.classes.removeWhere((s) => s.startsWith("--"));
-    currentTooltip.style.top=null;
-    currentTooltip.style.left=null;
-    currentTooltip.style.bottom=null;
-    currentTooltip.style.right=null;
-    currentTooltip.style.transform=null;
-
+    currentTooltip.style.top = null;
+    currentTooltip.style.left = null;
+    currentTooltip.style.bottom = null;
+    currentTooltip.style.right = null;
+    currentTooltip.style.transform = null;
 
     // musi se do toho zahrnout i pripadny zoom
-    double zoom = double.tryParse(itemElement.getComputedStyle().zoom)??1;
+    double zoom = double.tryParse(itemElement.getComputedStyle().zoom) ?? 1;
 
     parentBoundingBox = new Rectangle(
-      parentBoundingBox.left * zoom,
-      parentBoundingBox.top * zoom,
-      parentBoundingBox.width * zoom,
-      parentBoundingBox.height * zoom
-    );
+        parentBoundingBox.left * zoom,
+        parentBoundingBox.top * zoom,
+        parentBoundingBox.width * zoom,
+        parentBoundingBox.height * zoom);
 
     Point center = new Point(
-        parentBoundingBox.left + (parentBoundingBox.right - parentBoundingBox.left)/2,
-        parentBoundingBox.top + (parentBoundingBox.bottom - parentBoundingBox.top)/2
-    );
+        parentBoundingBox.left +
+            (parentBoundingBox.right - parentBoundingBox.left) / 2,
+        parentBoundingBox.top +
+            (parentBoundingBox.bottom - parentBoundingBox.top) / 2);
     if (center.x < window.innerWidth * 0.1) {
       // jsme hodne u kraje vlevo
       currentTooltip.classes.add("--right");
       currentTooltip.style.left = "${parentBoundingBox.right + 20}px";
       currentTooltip.style.top = "${center.y}px";
-      currentTooltip.style.transform="translateY(-50%)";
+      currentTooltip.style.transform = "translateY(-50%)";
     } else if (center.x > window.innerWidth * 0.9) {
       // jsme hodne u kraje vpravo
       currentTooltip.classes.add("--left");
-      currentTooltip.style.right="${window.innerWidth - parentBoundingBox.left + 20}px";
+      currentTooltip.style.right =
+          "${window.innerWidth - parentBoundingBox.left + 20}px";
       currentTooltip.style.top = "${center.y}px";
-      currentTooltip.style.transform="translateY(-50%)";
+      currentTooltip.style.transform = "translateY(-50%)";
     } else {
       // nejsme moc u kraje, dame se nahoru nebo dolu
       if (center.y < window.innerHeight / 2) {
         // jsme nahore
         currentTooltip.classes.add("--bottom");
         currentTooltip.style.top = "${parentBoundingBox.bottom + 20}px";
-        currentTooltip.style.left="${center.x}px";
-        currentTooltip.style.transform="translateX(-50%)";
+        currentTooltip.style.left = "${center.x}px";
+        currentTooltip.style.transform = "translateX(-50%)";
       } else {
         currentTooltip.classes.add("--top");
-        currentTooltip.style.bottom = "${window.innerHeight - parentBoundingBox.top + 20}px";
-        currentTooltip.style.left="${center.x}px";
-        currentTooltip.style.transform="translateX(-50%)";
+        currentTooltip.style.bottom =
+            "${window.innerHeight - parentBoundingBox.top + 20}px";
+        currentTooltip.style.left = "${center.x}px";
+        currentTooltip.style.transform = "translateX(-50%)";
       }
     }
   }
@@ -138,5 +134,4 @@ class FnxTooltip implements OnInit, OnDestroy {
       currentTooltip = null;
     }
   }
-
 }
